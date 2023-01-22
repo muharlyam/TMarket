3b3ecfaf67241ec7312391aed637333be16c8a9d
ceasar13m <ceasar13m@gmail.com>
2023-01-22 22:00:17 +0300

    [MPA-004] product fragment

--- app/src/main/java/com/muharlyam/tmarket/ui/ProductFragment.java
+++ app/src/main/java/com/muharlyam/tmarket/ui/ProductFragment.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
@@ -14,18 +16,29 @@ import com.muharlyam.tmarket.dto.ProductDto;
 
 public class ProductFragment extends Fragment {
 
+    FragmentProductBinding binding;
+
     public ProductFragment(){
         super(R.layout.fragment_product);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_product, container, false);
-        ProductDto productDto = new ProductDto();
-
-        FragmentProductBinding binding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_product, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ProductDto productDto = new ProductDto();
+        productDto.setName(getArguments().getString("name"));
+        productDto.setDescription(getArguments().getString("description"));
+        productDto.setImageUrl(getArguments().getString("imageUrl"));
+        productDto.setPrice(getArguments().getDouble("price"));
+        productDto.setRank(getArguments().getDouble("rank"));
+
         binding.setProduct(productDto);
-        return rootView;
     }
 }
